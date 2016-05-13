61fa47e5ef93a119d6dff13639d280272cd38cc4
Gustavo Bordoni <bordoni.dev@gmail.com>
2016-05-13 14:36:07 -0300

    Improve to handle better all the possible cases
    Add more fields that it will handle and doesn't break the JS

--- modern-tribe-central-links.js
+++ modern-tribe-central-links.js
@@ -12,21 +12,32 @@ var central_links = {};
 
 ( function( $, my ) {
     my.init = function() {
-        my.$pr_heading = $( 'table.attributes th:contains(Pull Request:)' );
-        var $link_cell = my.$pr_heading.next();
-        var link = $link_cell.html();
-        $link_cell.html( '<a href="' + link + '" target="_blank">' + link + '</a>' );
-        
-        my.$forum_heading = $( 'table.attributes th:contains(Forum threads:)' );
-        $link_cell = my.$forum_heading.next();
-        var links = $link_cell.html().split( "<br>" );
-        for ( var i in links ) {
-            links[i] = String( links[i] ).replace( /(http[^\n <]+)/, '<a href="$1" target="_blank">$1</a>' );
-        }
+        my.$headings = $( 'table.attributes' ).find( 'th:contains(Pull Request:), th:contains(Forum threads:), th:contains(User Story:), th:contains(UserVoice Threads:)' );
 
-        $link_cell.html( links.join( '<br>' ) );
+        my.$headings.each( function() {
+            var $this = $( this ),
+                $link_cell = $this.next( 'td' ),
+                link = $link_cell.html();
+
+            if ( ! link ) {
+                return;
+            }
+
+            var links;
+            if ( link.indexOf( '<br>' ) ) {
+                links = link.split( '<br>' );
+            } else {
+                links = link.split( ' ' );
+            }
+
+            for ( var i in links ) {
+                links[i] = String( links[i] ).replace( /(http[^\n <]+)/, '<a href="$1" target="_blank">$1</a>' );
+            }
+
+            $link_cell.html( links.join( '<br>' ) );
+        } );
     };
-    
+
     $( function() {
         my.init();
     });
