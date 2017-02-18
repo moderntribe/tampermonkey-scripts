36fd6f96c5828dc95179a85faa8772265b869563
Daniel Dvorkin <daniel@tri.be>
2017-02-18 03:51:38 -0300

    Add support for key selection on projects filter

--- modern-tribe-central-projects-filter.js
+++ modern-tribe-central-projects-filter.js
@@ -10,7 +10,18 @@
 
 ( function( $ ) {
 
-var container = $('li#projects-menu ul');
+var container = $('li#projects-menu ul'),
+    items = container.find('li');
+
+var add_style = function(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+};
 
 var filter = function(value){
     container.find('li').each(function(){
@@ -25,9 +36,31 @@ var filter = function(value){
     });
 }
 
+var handle_key = function(e){
+    var selected = $(".selected");
+    items.removeClass("selected");
+
+    switch(e.which){
+        case 38: //up
+            var prev = selected.prev(':visible');
+            prev.length ? prev.addClass("selected") : selected.siblings(':visible').last().addClass("selected")
+            break;
+        case 40: //down
+            var next = selected.next(':visible');
+            next.length ? next.addClass("selected") : selected.siblings(':visible').eq(1).addClass("selected");
+            break;
+        case 13: //enter
+            var link = selected.find('a').first();
+            if (link.length) window.location = link.attr('href');
+            break;
+        default:
+            filter(this.value);
+            container.find('li:visible').eq(1).addClass('selected');
+    }
+};
 
 
-var add_field = function() {
+var add_search_field = function() {
     var search_field = $('<input/>').attr({ type: 'text', id: 'test', name: 'test'});
     var li = $('<li />').append(search_field);
 
@@ -36,17 +69,14 @@ var add_field = function() {
     li.css('text-align', 'center');
     li.attr('data-project-filter', 1);
 
-    search_field.keyup(function(e){
-        if (e.which !== 13) filter(this.value);
-        else{
-            var link = container.find('li:visible').eq(1).find('a').first();
-            if (link.length) window.location = link.attr('href');
-        }
-    });
+    container.find('li').eq(1).addClass('selected');
+
+    search_field.keyup(handle_key);
 
     container.prepend(li);
 };
 
-add_field();
+add_style('#projects-menu ul li.selected a { background-color:#21A6CB !important; }');
+add_search_field();
 
 } )( jQuery );
