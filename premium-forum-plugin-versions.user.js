caf8aafe4558a189f13d5ffc2a6b7f294205d753
Andras Guseo <andras@divi-magazine.com>
2018-06-08 14:25:47 +0200

    Sysinfo from replies and eCommerce
    If sysinfo was submitted as a reply, then it is being pulled from there.
    If not, still from the sysinfo box.
    Script also checks for Woo and EDD versions.

--- premium-forum-plugin-versions.user.js
+++ premium-forum-plugin-versions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Premium Forum Extras - Latest plugin versions
 // @namespace    https://theeventscalendar.com/
-// @version      0.8
+// @version      0.9
 // @description  Display our plugins' latest version numbers (manually updated) and the user's version numbers from sysinfo
 // @author       Andras Guseo
 // @include      https://theeventscalendar.com/wp-admin/post.php?*
@@ -21,10 +21,31 @@
         $('.alwayson').css({ 'display': 'table-row'});
     }
 
+    // Current eCommerce versions
+    var currwoover = "3.4.2";
+    var curreddver = "2.9.3";
+    var ecmUsed, userEcmVer;
+
+    var i, k, inReply, replyHtml;
     // Getting the system info in a string
-    var i = document.getElementsByClassName( 'system-info' );
+    i = document.getElementsByClassName( 'system-info' );
     console.log('one' + i.length);
-    var fullstats = i[0].innerHTML;
+    if ( i.length > 0 ) {
+        var fullstats = i[0].innerHTML;
+    }
+
+    // If system information is submitted in a reply, take that instead
+    // Go from the last reply forward
+    var replies = document.getElementsByClassName( 'bbp-reply-content' );
+    for ( k = replies.length-1; k >= 0; k--) {
+        replyHtml = replies[k].innerHTML;
+        inReply = replyHtml.search( /home url/i );
+        console.log( k + 'two' + inReply );
+        if ( inReply >= 0 ) {
+            fullstats = replyHtml;
+            break;
+        }
+    }
 
     // Defining our plugins
     var pluginVersions = {
@@ -38,6 +59,8 @@
         fib: { name: "The Events Calendar: Filter Bar version ", version: "", curr: "#currfibver", user: "#userfibver" },
         apm: { name: "Advanced Post Manager version ", version: "", curr: "#currapmver", user: "#userapmver" },
         iwp: { name: "Image Widget Plus version ", version: "", curr: "#curriwpver", user: "#useriwpver" },
+        woo: { name: "WooCommerce version ", version:"", curr: "#currecmver", user: "#userecmver" },
+        edd: { name: "Easy Digital Downloads version ", version:"", curr: "#currecmver", user: "#userecmver" }
     };
 
     var j, pname, pby, pver;
@@ -53,13 +76,17 @@
             pname = fullstats.indexOf( pluginVersions[key].name );
             if ( pname != -1 ) {
                 // start of plugin name + plugin name length = start of version number
-                pname = fullstats.indexOf( pluginVersions[key].name )+ pluginVersions[key].name.length;
+                pname = fullstats.indexOf( pluginVersions[key].name ) + pluginVersions[key].name.length;
                 // start of by | end of version number
                 pby = fullstats.indexOf( "by", pname );
                 // version number only
-                pver = fullstats.substring ( pname, pby );
+                pver = fullstats.substring( pname, pby );
                 // trim it
                 pluginVersions[key].version = pver.trim();
+                // eCommerce used?
+                if ( key == "woo" || key == "edd" ) {
+                    ecmUsed = key;
+                }
             }
             // If plugin doesn't exist
             else {
@@ -74,16 +101,31 @@
     htmlstring += '<style>.versions td { padding: 0 5px !important; border-right: 1px solid white;line-height: 1.5em !important; font-size: 90% !important; } .versions td.new-version{ background-color:grey; } .versions td img {width: 40px !important;} .versions tr.first-row td {text-align: center;}</style>';
     htmlstring += '<table width="100%" class="versions" cellpadding="0" cellspacing="0">';
 
-    htmlstring += '<tr class="row first-row alwayson"><td></td><td></td><td><img src="https://andrasguseo.com/images/tec.png" title="TEC" alt="TEC" /></td><td><img src="https://andrasguseo.com/images/ecpro.png" title="PRO" alt="PRO" /></td><td><img src="https://andrasguseo.com/images/et.png" title="ET" alt="ET" /></td><td><img src="https://andrasguseo.com/images/et+.png" title="ET+" alt="ET+" /></td><td><img src="https://andrasguseo.com/images/ebt.png" title="Eventbrite" alt="Eventbrite" /></td><td><img src="https://andrasguseo.com/images/ce.png" title="CommEvents" alt="CommEvents" /></td><td><img src="https://andrasguseo.com/images/ct.png" title="CommTix" alt="CommTix" /></td><td><img src="https://andrasguseo.com/images/fb.png" title="Filter Bar" alt="Filter Bar" /></td><td>APM</td><td>IW+</td></tr>';
+    htmlstring += '<tr class="row first-row alwayson"><td></td><td></td><td><img src="https://andrasguseo.com/images/tec.png" title="TEC" alt="TEC" /></td><td><img src="https://andrasguseo.com/images/ecpro.png" title="PRO" alt="PRO" /></td><td><img src="https://andrasguseo.com/images/et.png" title="ET" alt="ET" /></td><td><img src="https://andrasguseo.com/images/et+.png" title="ET+" alt="ET+" /></td><td><img src="https://andrasguseo.com/images/ebt.png" title="Eventbrite" alt="Eventbrite" /></td><td><img src="https://andrasguseo.com/images/ce.png" title="CommEvents" alt="CommEvents" /></td><td><img src="https://andrasguseo.com/images/ct.png" title="CommTix" alt="CommTix" /></td><td><img src="https://andrasguseo.com/images/fb.png" title="Filter Bar" alt="Filter Bar" /></td><td>APM</td><td>IW+</td><td>';
+    htmlstring += ecmUsed.toUpperCase();
+    htmlstring += '</td></tr>';
 
-    htmlstring += '<tr class="row"><td>Mar 28</td><td>M18.05</td><td class="new-version">4.6.13</td><td>4.4.24.2</td><td class="new-version">4.7.1</td><td class="new-version">4.7.1</td><td>4.4.9</td><td class="new-version">4.5.10</td><td class="new-version">4.5.4</td><td>4.5.4</td>	<td>4.4</td><td>1.0.2</td></tr>';
+    //htmlstring += '<tr class="row"><td>Mar 28</td><td>M18.05</td><td class="new-version">4.6.13</td><td>4.4.24.2</td><td class="new-version">4.7.1</td><td class="new-version">4.7.1</td><td>4.4.9</td><td class="new-version">4.5.10</td><td class="new-version">4.5.4</td><td>4.5.4</td>	<td>4.4</td><td>1.0.2</td></tr>';
     htmlstring += '<tr class="row"><td> Apr 4</td><td>M18.06</td><td class="new-version">4.6.14.1</td><td class="new-version">4.4.25</td>	<td class="new-version">4.7.2</td>	<td class="new-version">4.7.2</td>	<td>4.4.9</td>	<td class="new-version">4.5.11</td>	<td>4.5.4</td>	<td class="new-version">4.5.5</td>	<td>4.4</td>	<td>1.0.2</td>	</tr>';
     htmlstring += '<tr class="row"><td> May 9</td><td>M18.07</td><td class="new-version">4.6.15</td><td class="new-version">4.4.26</td>	<td>4.7.2</td>	<td>4.7.2</td>	<td>4.4.9</td>	<td>4.5.11</td>	<td>4.5.4</td>	<td>4.5.5</td>	<td>4.4</td>	<td>1.0.2</td>	</tr>';
     htmlstring += '<tr class="row"><td>May 16</td><td>TEC   </td><td class="new-version">4.6.16</td><td>4.4.26</td>	<td>4.7.2</td>	<td>4.7.2</td>	<td>4.4.9</td>	<td>4.5.11</td>	<td>4.5.4</td>	<td>4.5.5</td>	<td>4.4</td>	<td>1.0.2</td>	</tr>';
-    htmlstring += '<tr class="row last-row alwayson"><td>May 29</td><td>M18.08</td><td class="new-version" id="currtecver">4.6.17</td><td id="currprover" class="new-version">4.4.27</td><td class="new-version" id="curretiver">4.7.3.1</td><td id="curretpver" class="new-version">4.7.3</td><td id="currebtver">4.4.9</td><td id="currcevver" class="new-version">4.5.12</td><td id="currctxver">4.5.4</td><td id="currfibver" class="new-version">4.5.6</td><td id="currapmver">4.4</td><td id="curriwpver">1.0.2</td></tr>';
+    htmlstring += '<tr class="row"><td>May 29</td><td>M18.08</td><td class="new-version">4.6.17</td><td class="new-version">4.4.27</td><td class="new-version">4.7.3.1</td><td class="new-version">4.7.3</td><td>4.4.9</td><td class="new-version">4.5.12</td><td>4.5.4</td><td class="new-version">4.5.6</td><td>4.4</td><td>1.0.2</td></tr>';
+    htmlstring += '<tr class="row last-row alwayson"><td>Jun 4</td><td>ETR</td><td class="new-version" id="currtecver">4.6.18</td><td id="currprover">4.4.27</td><td id="curretiver">4.7.3.1</td><td id="curretpver">4.7.3</td><td class="new-version" id="currebtver">4.5</td><td id="currcevver">4.5.12</td><td id="currctxver">4.5.4</td><td id="currfibver">4.5.6</td><td id="currapmver">4.4</td><td id="curriwpver">1.0.2</td><td id="currecmver">';
+    if ( ecmUsed == "woo" ) {
+        htmlstring += currwoover;
+        userEcmVer = pluginVersions.woo.version
+    }
+    else if ( ecmUsed == "edd" ) {
+        htmlstring += curreddver;
+        userEcmVer = pluginVersions.edd.version
+    }
+    else {
+        htmlstring += '-';
+    }
+    htmlstring += '</td></tr>';
 
     // Row of user's version numbers
-    htmlstring += '<tr class="row last-row alwayson userrow"><td>user</td><td>' + $( '#bbps_extra_info .displayname' ).html() + '</td><td id="usertecver">' + pluginVersions.tec.version + '</td><td id="userprover">' + pluginVersions.pro.version + '</td><td id="useretiver">' + pluginVersions.eti.version + '</td><td id="useretpver">' + pluginVersions.etp.version + '</td><td id="userebtver">' + pluginVersions.ebt.version + '</td><td id="usercevver">' + pluginVersions.cev.version + '</td><td id="userctxver">' + pluginVersions.ctx.version + '</td><td id="userfibver">' + pluginVersions.fib.version + '</td><td id="userapmver">' + pluginVersions.apm.version + '</td><td id="useriwpver">' + pluginVersions.iwp.version + '</td></tr>';
+    htmlstring += '<tr class="row last-row alwayson userrow"><td>user</td><td>' + $( '#bbps_extra_info .displayname' ).html() + '</td><td id="usertecver">' + pluginVersions.tec.version + '</td><td id="userprover">' + pluginVersions.pro.version + '</td><td id="useretiver">' + pluginVersions.eti.version + '</td><td id="useretpver">' + pluginVersions.etp.version + '</td><td id="userebtver">' + pluginVersions.ebt.version + '</td><td id="usercevver">' + pluginVersions.cev.version + '</td><td id="userctxver">' + pluginVersions.ctx.version + '</td><td id="userfibver">' + pluginVersions.fib.version + '</td><td id="userapmver">' + pluginVersions.apm.version + '</td><td id="useriwpver">' + pluginVersions.iwp.version + '</td><td id="userecmver">' + userEcmVer + '</td></tr>';
     htmlstring += '</table>';
     htmlstring += '</div>';
 
@@ -94,7 +136,6 @@
     $('.version-number').css({ 'font-weight': 'bold' });
     $('.row').css({ 'display': 'none', 'text-align': 'center' });
     $('.alwayson').css({ 'display': 'table-row'});
-    //jQuery('.userrow').css({ 'text-align': 'right'});
 
     // Handle hover
     if ( document.getElementById('plugin-versions') != null ) {
