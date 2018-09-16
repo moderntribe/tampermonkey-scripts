4ac4370d06c71fc17a5337d575a5f3397eaa4df8
Clifford <tko@tourkick.com>
2018-09-16 05:49:43 -0500

    Add download/update link
    so Tampermonkey can auto-update

--- key-commands.js
+++ key-commands.js
@@ -5,6 +5,7 @@
 // @description  Add some basic key commands to central.
 // @author       Gary Kovar
 // @match        *://central.tri.be/issues*
+// @downloadURL  https://raw.githubusercontent.com/moderntribe/tampermonkey-scripts/master/key-commands.js
 // @grant        none
 // ==/UserScript==
 
