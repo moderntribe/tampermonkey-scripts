ab20cd54ae8b9fee54c57b6ac49ce834fa5a1666
Andras Guseo <andras@tri.be>
2019-01-24 08:06:07 +0100

    KB19.01 version numbers

--- liveagent-plugin-versions.user.js
+++ liveagent-plugin-versions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LiveAgent - Latest plugin versions
 // @namespace    https://theeventscalendar.com/
-// @version      2.1.0
+// @version      2.2.0
 // @description  Display our plugins' latest version numbers.
 // @author       Andras Guseo
 // @include      https://theeventscalendar.ladesk.com/agent/*
@@ -74,9 +74,10 @@
             19: { note: "",     date: "Nov 13", name: "M18.16", tec: "4.6.26.1x", pro: "4.4.35x",   eti: "4.8.4.1x", etp: "4.8.3",  ebt: "4.5.6x", cev: "4.5.14x",   ctx: "4.5.6",  fib: "4.5.8",  apm: "4.4", iwp: "1.0.2", woo: "3.5.1", edd: "2.9.8" },
             20: { note: "",     date: "Nov 29", name: "F18.03", tec: "4.7.0.1x",  pro: "4.5x",      eti: "4.9.0.2x", etp: "4.9x",   ebt: "4.5.6",  cev: "4.5.14",    ctx: "4.5.6",  fib: "4.5.8",  apm: "4.4", iwp: "1.0.2", woo: "3.5.2", edd: "2.9.8" },
             21: { note: "",     date: "Dec 5",  name: "M18.17", tec: "4.7.1x",    pro: "4.5",       eti: "4.9.1x",   etp: "4.9",    ebt: "4.5.6",  cev: "4.5.15x",   ctx: "4.5.7x", fib: "4.5.9x", apm: "4.4", iwp: "1.0.2", woo: "3.5.2", edd: "2.9.9" },
-            22: { note: "show", date: "Dec 13", name: "M18.18", tec: "4.7.2x",    pro: "4.5.1x",    eti: "4.9.2x",   etp: "4.9",    ebt: "4.5.6",  cev: "4.5.15",    ctx: "4.5.7",  fib: "4.5.9",  apm: "4.4", iwp: "1.0.2", woo: "3.5.2", edd: "2.9.10" },
+            22: { note: "",     date: "Dec 13", name: "M18.18", tec: "4.7.2x",    pro: "4.5.1x",    eti: "4.9.2x",   etp: "4.9",    ebt: "4.5.6",  cev: "4.5.15",    ctx: "4.5.7",  fib: "4.5.9",  apm: "4.4", iwp: "1.0.2", woo: "3.5.2", edd: "2.9.10" },
             23: { note: "show", date: "Dec 19", name: "M18.19", tec: "4.7.3x",    pro: "4.5.2.1x",  eti: "4.9.3x",   etp: "4.9.1x", ebt: "4.5.6",  cev: "4.5.15",    ctx: "4.5.7",  fib: "4.5.9",  apm: "4.4", iwp: "1.0.2", woo: "3.5.3", edd: "2.9.10" },
-            24: { note: "last", date: "Jan 15", name: "G19.01", tec: "4.7.3",     pro: "4.5.2.1",   eti: "4.9.4x",   etp: "4.9.2x", ebt: "4.5.6",  cev: "4.5.16x",   ctx: "4.5.7",  fib: "4.5.9",  apm: "4.4", iwp: "1.0.2", woo: "3.5.3", edd: "2.9.10" },
+            24: { note: "show", date: "Jan 15", name: "G19.01", tec: "4.7.3",     pro: "4.5.2.1",   eti: "4.9.4x",   etp: "4.9.2x", ebt: "4.5.6",  cev: "4.5.16x",   ctx: "4.5.7",  fib: "4.5.9",  apm: "4.4", iwp: "1.0.2", woo: "3.5.3", edd: "2.9.10" },
+            25: { note: "last", date: "Jan 21", name: "B19.01", tec: "4.7.4x",    pro: "4.5.3x",    eti: "4.9.4",    etp: "4.9.2",  ebt: "4.5.7x", cev: "4.5.16",    ctx: "4.5.7",  fib: "4.5.9",  apm: "4.4", iwp: "1.0.2", woo: "3.5.4", edd: "2.9.11" },
         };
 
         //var pluginNames = ['tec', 'pro', 'eti', 'etp', 'ebt', 'cev', 'ctx', 'fib', 'apm', 'iwp'];
