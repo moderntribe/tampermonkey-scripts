c247bb13222a8987f1afa1d92f682fcbdfd72f83
Peter Chester <peter@tri.be>
2019-06-20 11:30:33 -0700

    revert

--- easyredmine-skin.js
+++ easyredmine-skin.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EasyRedmine
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.easyredmine.com/*
@@ -368,9 +368,9 @@ span[data-value="35"] {
     display: none;
 }
 
-/*span.entity-array {
+span.entity-array {
     display: none;
-}*/
+}
 
 .easy-query-filter-container .buttons>div span {
     display: none;
