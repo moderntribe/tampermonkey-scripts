d08e56bdfc0fa78dab8dc87d21ca17558967f86f
Clifford Paulick <tko@tourkick.com>
2019-12-16 13:07:12 -0600

    get it working for some more URLs, with Ajax

--- jira-remove-harvest.user.js
+++ jira-remove-harvest.user.js
@@ -2,17 +2,31 @@
 // @name         Jira: Remove Harvest time tracking widget
 // @namespace    https://moderntribe.atlassian.net/
 // @version      0.0.1
-// @description  The applicable template inserts into the empty Description field after selecting an Issue Tracker status (Bug, Feature, etc.)
+// @description  Remove the "Harvest Time Tracking" widget from the View Issue Sidebar.
 // @author       Clifford Paulick
 // @include      https://moderntribe.atlassian.net/browse/*
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/moderntribe/tampermonkey-scripts/master/jira-remove-harvest.user.js
+// @require      https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
+// @require      https://raw.githubusercontent.com/moderntribe/tampermonkey-scripts/master/waitForKeyElements.js
 // ==/UserScript==
 
-(function () {
+let jira_remove_harvest_sidebar_widget = {};
+
+(function ( $, app ) {
 	'use strict';
 
-	let jira_harvest_sidebar_widget = document.querySelector( '#viewissuesidebar > #harvest__toggle-timer' );
+	/**
+	 * Remove the "Harvest Time Tracking" widget from the View Issue Sidebar, only if detected / after loaded.
+	 *
+	 * @see waitForKeyElements() The third-party script (requires jQuery) that fires our function only if/after the
+	 *                           selector is found, such as after an Ajax call. Required to work at some of the Jira URLs.
+	 *
+	 * @param e
+	 */
+	function remove_harvest_widget( e ) {
+		e.remove();
+	}
 
-	jira_harvest_sidebar_widget.parentNode.removeChild( jira_harvest_sidebar_widget );
-})();
+	waitForKeyElements( '#viewissuesidebar > #harvest__toggle-timer', remove_harvest_widget );
+})( jQuery, jira_remove_harvest_sidebar_widget );
